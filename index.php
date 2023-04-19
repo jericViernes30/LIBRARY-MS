d04b453ddede502eea2eb460a88e6f99d7067062
Jeric Viernes <jericviernes06@gmail.com>
2023-04-19 20:18:44 +0800

    landing page

--- index.php
+++ index.php
@@ -12,13 +12,9 @@
     <div class="display">
         <div class="login-container">
             <h3>SVCC LIBRARY</h3>
-
-
             <div class="login-form">
                 <h3>LOGIN</h3>
-
-
-                <form action="<?php echo $_SERVER['PHP_SELF'] ?>" method="POST">
+                <form action="home1.php" method="POST">
                     <div class="input">
                         <input type="text" name="username" required="required">
                         <span>Username</span>
@@ -29,20 +25,9 @@
                     </div>
                     <button class="login-button" name="btn">LOGIN</button>
                 </form>
-
-
             </div>
-
-
-
         </div>
         <div class="logo"><img src="img/svcc-logo.png" alt="SVCC Logo"></div>
     </div>
-        
-    <?php
-        if(isset($_POST['btn'])) {
-            echo $_POST['username'];
-        }
-    ?>
 </body>
 </html>
